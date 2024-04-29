cbf5154483182f56775d6505f97aa3e79b0db9f5
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-04-29 18:42:49 +0530

    Server Created

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const express=require("express");
+const express = require("express");
+const { connectDatabase } = require("./config/database");
 require("dotenv").config();
 
-const PORT=process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-const app=express();
+const app = express();
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
+    connectDatabase();
     console.log(`Server Started at PORT: ${PORT}`);
 })
