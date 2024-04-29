c0efd41c0ba7b59fa03c85ab8710ffd2cd8d9010
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-04-29 19:12:41 +0530

    Server Created

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const { connectDatabase } = require("./config/database");
+const userRoutes = require("./routes/User");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
-
 const app = express();
+
+app.use(express.json());
+app.use("/user", userRoutes);
 
 app.listen(PORT, () => {
     connectDatabase();
