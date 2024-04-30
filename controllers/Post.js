3627e82eabb6d5b83b4092c3f1c147e85e0ebd52
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-05-01 00:23:42 +0530

    Working on Auth Functionality

--- controllers/Post.js
+++ controllers/Post.js
@@ -1,13 +1,13 @@
 const cloudinary = require("cloudinary").v2;
 const fs = require('fs');
+const Post = require("../models/Post");
+const User = require("../models/User");
 
 exports.createPost = async (req, res) => {
     try {
         const folderName = "Social Media";
         const image = req.file.path;
-
-        console.log(req.file);
-        console.log(image);
+        console.log("Consoling Req.File ->", req.file);
 
         const response = await cloudinary.uploader.upload(image,
             {
@@ -15,17 +15,44 @@ exports.createPost = async (req, res) => {
                 resource_type: "auto"
             }
         )
-        console.log(response);
-
 
         fs.unlink(image, function (err) {
             if (err) throw err;
             console.log('File deleted!');
         });
 
+        const { caption, user } = req.file;
+
+        const newPost = await Post.create({
+            imageUrl: response.secure_url,
+            caption
+        })
+        console.log("New Post Creted")
+
+        const updatedUser=null;
+        try {
+            updatedUser = await User.findOneAndUpdate(
+                { username: user },
+                {
+                    $push: {
+                        posts: newPost._id
+                    }
+                },
+                { new: true }
+            )
+        }
+        catch (error) {
+            console.log(error);
+            res.status(500).json({
+                message: "An Error Occurred While Saving User Data"
+            })
+        }
+        console.log("User Updated");
+
         res.json({
             success: true,
-            imageUrl: response.secure_url
+            data: newPost,
+            user: updatedUser
         })
 
     }
