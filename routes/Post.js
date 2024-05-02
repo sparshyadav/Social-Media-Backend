90a49f7b3460f797f92a1ee8f9b1bb583eeef3a7
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-05-02 17:07:12 +0530

    Routes Created for Post Controller

--- routes/Post.js
+++ routes/Post.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const multer = require("multer");
 const { createPost } = require("../controllers/Post");
-const storage=require("../config/multer");
+const storage = require("../config/multer");
+const { auth } = require("../middlewares/auth");
 const router = express.Router();
 const upload = multer({ storage: storage });
 
-router.post("/create", upload.single("image"), createPost);
+router.post("/create", auth, upload.single("image"), createPost);
 
 module.exports = router;
