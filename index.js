fc68698924faa1515967dc122374d36e9bbcb3bb
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-05-02 16:01:12 +0530

    Index File Updated with Cookie-Parser

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const { connectDatabase } = require("./config/database");
 const userRoutes = require("./routes/User");
 const postRoutes=require("./routes/Post");
 const cloudinaryConnect = require("./config/cloudinary");
+const cookieParser = require('cookie-parser');
 require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
+
 app.use("/user", userRoutes);
 app.use("/post", postRoutes);
 
