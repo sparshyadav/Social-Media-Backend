a5ad28e8a38ccd733b1297e874230a70802eeb89
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-05-02 16:00:07 +0530

    Post Controller Created

--- controllers/Post.js
+++ controllers/Post.js
@@ -2,67 +2,56 @@ const cloudinary = require("cloudinary").v2;
 const fs = require('fs');
 const Post = require("../models/Post");
 const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 exports.createPost = async (req, res) => {
     try {
+        // Taking Folder Name on Cloudinary, and File Path of the File
         const folderName = "Social Media";
         const image = req.file.path;
-        console.log("Consoling Req.File ->", req.file);
 
-        const response = await cloudinary.uploader.upload(image,
-            {
-                folder: folderName,
-                resource_type: "auto"
-            }
-        )
-
-        fs.unlink(image, function (err) {
-            if (err) throw err;
-            console.log('File deleted!');
+        // Uploading the File on Clodinary in the Requested Folder
+        const response = await cloudinary.uploader.upload(image, {
+            folder: folderName,
+            resource_type: "auto"
         });
 
-        const { caption, user } = req.file;
+        // Delete the local file after uploading to Cloudinary
+        fs.unlinkSync(image);
 
+        // Take Caption from the Request Body
+        const { caption } = req.body;
+
+        // Create a New Post
         const newPost = await Post.create({
             imageUrl: response.secure_url,
             caption
-        })
-        console.log("New Post Creted")
+        });
 
-        const updatedUser=null;
-        try {
-            updatedUser = await User.findOneAndUpdate(
-                { username: user },
-                {
-                    $push: {
-                        posts: newPost._id
-                    }
-                },
-                { new: true }
-            )
-        }
-        catch (error) {
-            console.log(error);
-            res.status(500).json({
-                message: "An Error Occurred While Saving User Data"
-            })
-        }
-        console.log("User Updated");
+        // Verify Cookie to Get User and Gather User ID
+        const decode = jwt.verify(req.cookies.token, process.env.JWT_SECRET);
+        const userId = decode.id;
+
+        // Update the User with Post ID
+        const updatedUser = await User.findByIdAndUpdate(
+            { _id: userId },
+            { $push: { Posts: newPost._id } },
+            { new: true }
+        ).populate("Posts").exec();
 
         res.json({
             success: true,
             data: newPost,
             user: updatedUser
-        })
-
-    }
-    catch (error) {
+        });
+    } catch (error) {
         console.log(error);
 
         return res.status(500).json({
             success: false,
             message: "An Error Occurred While Creating Post",
             error: error.message
-        })
+        });
     }
-}
+};
