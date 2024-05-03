6cf0ca3e7007c9cd8dcfe787f9eb857fde5f2690
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-05-03 15:28:39 +0530

    Single Route Created to Handle Like/Unline

--- routes/Post.js
+++ routes/Post.js
@@ -3,9 +3,11 @@ const multer = require("multer");
 const { createPost } = require("../controllers/Post");
 const storage = require("../config/multer");
 const { auth } = require("../middlewares/auth");
+const { likeUpdate } = require("../controllers/Like");
 const router = express.Router();
 const upload = multer({ storage: storage });
 
 router.post("/create", auth, upload.single("image"), createPost);
+router.post("/like/", auth, likeUpdate);
 
 module.exports = router;
