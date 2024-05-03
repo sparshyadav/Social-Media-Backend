07d9446d3c0a8620f8dcfa798b5b8771a0e117ad
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-05-03 16:04:26 +0530

    Router Created to Create a New Comment

--- routes/Post.js
+++ routes/Post.js
@@ -4,10 +4,12 @@ const { createPost } = require("../controllers/Post");
 const storage = require("../config/multer");
 const { auth } = require("../middlewares/auth");
 const { likeUpdate } = require("../controllers/Like");
+const { commentCreate } = require("../controllers/Comment");
 const router = express.Router();
 const upload = multer({ storage: storage });
 
 router.post("/create", auth, upload.single("image"), createPost);
-router.post("/like/", auth, likeUpdate);
+router.post("/like", auth, likeUpdate);
+router.post("/comment", auth, commentCreate);
 
 module.exports = router;
