10d20820ca9eebef8d86cf5e56571abcee3d5f21
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-05-04 11:37:17 +0530

    Routes Created for Comment Delete, and All Post Comments

--- routes/Post.js
+++ routes/Post.js
@@ -4,12 +4,15 @@ const { createPost } = require("../controllers/Post");
 const storage = require("../config/multer");
 const { auth } = require("../middlewares/auth");
 const { likeUpdate } = require("../controllers/Like");
-const { commentCreate } = require("../controllers/Comment");
+const { commentCreate, commentUpdate, commentDelete, commentPostAll } = require("../controllers/Comment");
 const router = express.Router();
 const upload = multer({ storage: storage });
 
 router.post("/create", auth, upload.single("image"), createPost);
 router.post("/like", auth, likeUpdate);
-router.post("/comment", auth, commentCreate);
+router.post("/comment/create", auth, commentCreate);
+router.put("/comment/update", auth, commentUpdate);
+router.delete("/comment/delete", auth, commentDelete);
+router.get("/comment/all", auth, commentPostAll);
 
 module.exports = router;
