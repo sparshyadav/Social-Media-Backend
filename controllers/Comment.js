6e829489eefdee31ee3ab03b7fba40e83e88aa91
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-05-04 11:35:39 +0530

    Comment Controller Updated with Update Comment, and Delete Comment Functions

--- controllers/Comment.js
+++ controllers/Comment.js
@@ -63,3 +63,125 @@ exports.commentCreate = async (req, res) => {
         })
     }
 }
+
+exports.commentUpdate = async (req, res) => {
+    try {
+        const { postId, commentId, comment } = req.body;
+        console.log("postID", postId);
+        console.log("postID", commentId);
+        console.log(comment)
+
+        if (!postId, !commentId || !comment) {
+            return res.status(401).json({
+                success: false,
+                message: "All Fields Are Required"
+            })
+        }
+
+        const updatedComment = await Comment.findOneAndUpdate(
+            { post: postId, _id: commentId },
+            {
+                $set: {
+                    text: comment
+                }
+            },
+            { new: true }
+        )
+
+        res.status(200).json({
+            success: true,
+            message: "Comment Updated Successfully",
+            comment: updatedComment
+        })
+    }
+    catch (error) {
+        console.log("An Error Occurred While Updating Comment", error);
+
+        res.status(500).json({
+            success: false,
+            message: "An Error Occurred While Updating Comment",
+            error: error.message
+        })
+    }
+}
+
+exports.commentDelete = async (req, res) => {
+    try {
+        const { postId, commentId } = req.body;
+        console.log("Post ID -", postId);
+        console.log("CommentId - ", commentId);
+
+        if (!postId || !commentId) {
+            return res.status(401).json({
+                success: false,
+                message: "All Fields are Required"
+            })
+        }
+        console.log("Both Validated");
+
+        const updatedPost = await Post.findByIdAndUpdate(
+            postId,
+            {
+                $pull: {
+                    comments: commentId
+                }
+            },
+            { new: true }
+        ).populate("comments").exec();
+        console.log("Updated Post Updated", updatedPost);
+
+        await Comment.findByIdAndDelete(commentId);
+
+
+        res.status(200).json({
+            success: true,
+            message: "Comment Deleted Successfully",
+            post: updatedPost
+        })
+    }
+    catch (error) {
+        console.log("An Error Occurred While Deleting the Comment");
+
+        res.status(500).json({
+            status: false,
+            message: "An Error Occurred While Deleting the Comment"
+        })
+    }
+}
+
+exports.commentPostAll = async (req, res) => {
+    try {
+        const { postId } = req.body;
+
+        if (!postId) {
+            return res.status(401).json({
+                success: false,
+                message: "All Fields are Required"
+            })
+        }
+
+        let allPostComments = await Comment.find({ post: postId });
+
+        if (!allPostComments) {
+            return res.status(401).json({
+                success: false,
+                message: "The Post Contains no Comments"
+            })
+        }
+
+        res.status(200).json({
+            success: true,
+            message: "All Comments of The Post Gathered",
+            comments: allPostComments
+        })
+    }
+    catch (error) {
+        console.log("An Error Occurred While Getting All The Comments of a Post", error);
+
+        res.status(500).json({
+            success: false,
+            message: "An Error Occurred While Getting All The Comments of a Post",
+            error: error.message
+        })
+    }
+}
